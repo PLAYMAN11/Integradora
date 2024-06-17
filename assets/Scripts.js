05202eb8a14032c16c66ae2f4cc17e43e529d4d0
PLAYMAN11 <EmmanuelGZHZ@hotmail.com>
2024-06-16 23:06:47 -0600

    Se cambia el diseño del index y se agrega una tabla con animacion para mero diseño

---
+++ assets/Scripts.js
@@ -0,0 +1,14 @@
+function Animacion() {
+    document.addEventListener('DOMContentLoaded', () => {
+        const bars = document.querySelectorAll('.bar');
+        bars.forEach(bar => {
+            const value = bar.style.getPropertyValue('--bar-value');
+            setTimeout(() => {
+                bar.style.width = value;
+            }, 100);
+        });
+    });
+}
+Animacion();
+
+
