82f0753adfda516ab0de6a1166a45bbdd6671971
PLAYMAN11 <EmmanuelGZHZ@hotmail.com>
2024-07-07 17:58:55 -0600

    Unificacion con el server para la creacion de usuarios

--- assets/Scripts.js
+++ assets/Scripts.js
@@ -1,15 +1,43 @@
-function Animacion() {
-    document.addEventListener('DOMContentLoaded', () => {
-        const bars = document.querySelectorAll('.bar');
-        bars.forEach(bar => {
-            const value = bar.style.getPropertyValue('--bar-value');
-            setTimeout(() => {
-                bar.style.width = value;
-            
-            },);
-        });
-    });
+
+function Registro(){
+    var nombre = document.getElementById("Nombre").value;
+    var apellido = document.getElementById("Apellido").value;
+    var correo = document.getElementById("Correo").value;
+    var contraseña = document.getElementById("Contraseña").value;
+    var telefono = document.getElementById("Telefono").value;
+    var fecha = document.getElementById("FechaNac").value;
+
+    if(nombre == "" || apellido == "" || correo == "" || contraseña == "" || telefono == "" || fecha == ""){
+        alert("Por favor llene todos los campos");
+    }else{
+        async function postData() {
+            try {
+            const response = await fetch('http://localhost:3000/usuarios/Registro', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "NombreUsuario": nombre,
+                    "Contraseña": contraseña,
+                    "ApellidoUsuario": apellido,
+                    "CorreoUsuario": correo,
+                    "Telefono": telefono,
+                    "FECHA_NACIMIENTO": fecha
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error("Error en la respuesta del servidor: " + response.status);
+            }
+            const data = await response.text(); 
+            alert("Registro completado con éxito");
+            window.location.href = "iniciarSesion.html";
+        } catch (error) {
+            console.error('Error:', error);
+            alert("Error al procesar la solicitud: " + error.message);
+        }
+    }
+    postData();
 }
-Animacion();
-
-
+}
