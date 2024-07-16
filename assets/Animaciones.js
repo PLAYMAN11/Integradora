a62e9f172cb4ba9348eb7d4e648643f00e9e48a5
PLAYMAN11 <EmmanuelGZHZ@hotmail.com>
2024-07-15 22:00:28 -0600

    Cambio de letras para que sean autoajustables, ajuste en el header main para acomodo y funcionalidad de los href

--- assets/Animaciones.js
+++ assets/Animaciones.js
@@ -1,4 +1,4 @@
-const { get } = require("http");
+
 
 function Animacion() {
     document.addEventListener('DOMContentLoaded', () => {
@@ -12,29 +12,48 @@ function Animacion() {
         });
     });
 }
+
+let bandera = true;
 function DisplayMenu() {
     const menu = document.getElementById("Desplegable");
     const navs = document.querySelectorAll('header nav div');
-    if(menu.style.opacity == "1"){
+    const lineas = document.querySelectorAll('.activadorHeaderMain');
+    if(bandera){
+        lineas.forEach(lineas => {
+            lineas.children[0].style.transform = "rotate(0deg)";
+            lineas.children[1].style.opacity = 1;
+            lineas.children[2].style.transform = "rotate(0deg)";
+    }); 
+    }else {
+        lineas.forEach(lineas => {
+            lineas.children[0].style.transform = "rotate(45deg)";
+            lineas.children[1].style.opacity = 0;
+            lineas.children[2].style.transform = "rotate(-45deg)";
+        });
+    }
+    bandera = !bandera;
+
+    if(menu.style.opacity=='0'){
+            menu.style.opacity = '1';
+            menu.style.display = "flex";
+            menu.style.justifyContent = "flex-start";
+            menu.style.margin = "0 5% 0 0%";
+            navs.forEach(navs => {
+                navs.style.margin = "0 1% 0 1%";
+        });
+
+    } else {
         let margen = -15;
-        menu.style.opacity = "0";
+        menu.style.opacity = '0';
         menu.style.justifyContent = "left";
         navs.forEach(navs => {
             navs.style.margin = `0 0 0 ${margen}%`;
             margen = margen - 10;
         });
-
-    } else {
-        menu.style.opacity = "1";
-        menu.style.display = "flex";
-        menu.style.justifyContent = "flex-start";
-        menu.style.margin = "0 5% 0 0%";
-        navs.forEach(navs => {
-            navs.style.margin = "0 1% 0 1%";
-        });
     
     }
 }
+
 
 
 
